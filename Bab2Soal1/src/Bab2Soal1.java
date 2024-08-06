1e60a27a68b5539baa0c76eedbdec9a5f9d4943a
slmazqhrmdhn <143584478+slmazqhrmdhn@users.noreply.github.com>
2024-08-06 15:25:34 +0700

    tugas

---
+++ Bab2Soal1/src/Bab2Soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab2Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+int a=50;
+float b=a;
+System.out.println(a);
+System.out.println(b);
+}} 
